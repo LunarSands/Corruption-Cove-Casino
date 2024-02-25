b61b6960d5d4d64033da602a9d37d90e3a4c58ee
Theo Sanderson <2787714s@student.gla.ac.uk>
2024-02-25 15:30:12 +0000

    finished nav-bar (still need to test logged in view)

--- Corruption_Cove/apps.py
+++ Corruption_Cove/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CorruptionCoveConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Corruption_Cove'
+# class CorruptionCoveConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'Corruption_Cove'
--- Corruption_Cove/views.py
+++ Corruption_Cove/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, "Corruption_Cove/index.html", context)
+
+def nav_bar(request):
+    context = {}
+    return render(request, "Corruption_Cove/nav.html", context)
