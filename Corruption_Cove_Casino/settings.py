8ff05be1e6340275ae2e524a684124e25c536db3
Alasdair Morrison <2742064m@student.gla.ac.uk>
2024-03-03 16:51:30 +0000

    basic registration (nonfunctional)

--- Corruption_Cove_Casino/settings.py
+++ Corruption_Cove_Casino/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Corruption_Cove',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Registration settings
+REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = 'Corruption_Cove:index'
+LOGIN_URL = 'auth_login'
