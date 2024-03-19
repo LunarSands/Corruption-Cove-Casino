8292cf9e5ecbbbbbe290369a66e59510eca0c84c
Danylo <danylo.kiz@gmail.com>
2024-03-19 21:40:49 +0000

    progressing with slots

--- Corruption_Cove/games/slots.py
+++ Corruption_Cove/games/slots.py
@@ -7,17 +7,48 @@ from django.http import HttpResponse, HttpRequest, JsonResponse, HttpResponseBad
 
 from Corruption_Cove.games.game import Game
 
+# Every slots bet costs 100 pounds.
+SLOTS_BET = 100
+# List of all slot elements.
+SLOTS_ELEMENTS = ['Cherry', 'Apple', 'Banana', 'Orange', 'Grapes']
+
 class Slots(Game):
     def __init__(self, state, user):
         super().__init__(state,user)
         self.set_state(state)
         self.name = 'slots'
+        self.result = None
 
-    def handle_start(self, action):
+    def handle_start(self, request):
         print('slots in process')
+
+        request_body = json.loads(request.body)
+        action = request_body.get('action')
+
+        print(self.calculate_win_or_loss())
 
     def client_state(self):
         return {}
+
+    def calculate_win_or_loss(self):
+        result1 = random.choice(SLOTS_ELEMENTS)
+        result2 = random.choice(SLOTS_ELEMENTS)
+        result3 = random.choice(SLOTS_ELEMENTS)
+
+        if result1 == result2 == result3:
+            if result1 == 'Cherry':
+                # Jackpot
+                return SLOTS_BET * 10
+            else:
+                # Regular
+                return SLOTS_BET * 5
+        elif result1 == result2 or result1 == result3 or result2 == result3:
+            # Partial
+            return SLOTS_BET * 1
+        else:
+            # Loss
+            return SLOTS_BET * -1
+
 
 
 @login_required
@@ -26,9 +57,8 @@ def slots(request):
         slots_game = Slots({},request.user)
 
         try:
-            action = json.loads(request.body)
             print('handling slots')
-            slots_game.handle_action(action)
+            slots_game.handle_action(request)
             print('handled slots')
         except ValueError as error:
             print(error)
