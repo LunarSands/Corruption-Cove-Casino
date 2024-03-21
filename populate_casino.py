8650a3fd6bb134f55c092d1227ee6741ca8e7f01
Vladislav Brusokas <brusvlad4002@yandex.ru>
2024-03-21 21:00:54 +0000

    Populate script amended to populate slots, slots are displayed and link to appropriate pages

--- populate_casino.py
+++ populate_casino.py
@@ -14,6 +14,10 @@ def populate():
                'Dick': {'face':'/media/images/dealers/Dick.png', 'stop':17, 'soft':False},
                'Jason':{'face':'/media/images/dealers/Jason.png', 'stop':19, 'soft':True},
             }
+    slots = {'Egypt': {'preview':'/media/images/slots/Egypt.png'}, 
+             'Classic': {'preview':'/media/images/slots/Classic.png'},
+             'Fruit':{'preview':'/media/images/slots/Fruit.png'},
+            }
     users = [{'name':f'user{x}','pass':f'pass{x}'} for x in range(10)]
     cards = [{'name':f'name{x}','no':random.choices('0123456789',k=16),'expiry':'10/26','cvv':random.choices('0123456789',k=3)} for x in range(10)]
 
@@ -24,11 +28,20 @@ def populate():
         print(f"Adding dealer: {dealer}")
         add_dealer(dealer, info['face'], info['stop'], info['soft'])
 
+    for machine,info in slots.items():
+        print(f"Adding slot machine: {machine}")
+        add_machine(machine, info['preview'])
+
 def add_dealer(name, face, stop, soft):
     dealer = Dealer.objects.get_or_create(name=name)[0]
     dealer.stop = stop
     dealer.soft = soft
     dealer.save()
+
+def add_machine(theme, preview):
+    machine = Slots.objects.get_or_create(theme=theme)[0]
+    machine.preview = preview
+    machine.save()
 
 def add_user(user,card):
     user_model = User.objects.get_or_create(username=user['name'])[0]
