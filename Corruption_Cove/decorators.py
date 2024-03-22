a2b643060005d52770f9d9da55709eaa8b3b76db
Gregor0410 <34042398+gregor0410@users.noreply.github.com>
2024-03-22 12:14:04 +0000

    fix card required decorator

--- Corruption_Cove/decorators.py
+++ Corruption_Cove/decorators.py
@@ -8,9 +8,10 @@ def card_required(function):
     def wrapper(request, *args, **kwargs):
         user = request.user
         user_prof = UserProfile.objects.get(user=user)
-        card = Bank.objects.get(username=user_prof)
-        if card is None:
-            return redirect(reverse('corruption-cove-casino:account') + f'/{user_prof.slug}')
+        try:
+            card = Bank.objects.get(username=user_prof)
+        except Bank.DoesNotExist:
+            return redirect(reverse('corruption-cove-casino:add_card',kwargs={'user_slug':user_prof.slug}))
         return function(request, *args, **kwargs)
 
     return wrapper
